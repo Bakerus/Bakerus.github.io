37c9c6defa4f61d1b839046db2972b9b04c3e750
William Bakehe <111873519+Bakerus@users.noreply.github.com>
2022-09-05 05:50:48 +0100

    mise en ligne

---
+++ Porte-folio/php/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="..\feuillesStyleCSS\portefolio.css">
+    <title>laMeute</title>
+  </head>
+  <body>
+
+  <header>
+        <nav class="navbar bg-light">
+
+          <div class="container-navbartitle">
+            <a class="navbar-title" href="#"> <span>Port</span><Span>Folio</Span></a>
+          </div>
+
+          <div class="contatainer-items">
+              <a class="navbar-items" href="#">About</a>
+              <a class="navbar-items" href="#">Our Projects</a>
+              <a class="navbar-items" href="#">Contact Us</a>
+              <a class="navbar-items" href="#">Skills</a>
+          </div>
+
+        </nav>
+
+        <Section>
+          <img src="..\assets\images\blackblue.png" alt="logo la meute" class="logo">
+          <aside>
+            <p> LAMEUTE</p>
+          </aside>
+          <aside>
+            <p class="logoname"> We are a group specialized in computer service such as software programming web programming , mobile app builds, graphic design, 3D modeling and counting... </p>
+          </aside>
+        </Section>
+      <footer>
+        <img src="..\assets\icons\Facebook.png" alt="">
+        <img src="..\assets\icons\Instagram.png" alt="">
+        <img src="..\assets\icons\Twitter.png" alt="">
+        <img src="..\assets\icons\WhatsApp.png" alt="">
+      </footer>
+  </header>
+
+  </body>
+</html>
---
+++ Porte-folio/php/test.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="..\feuillesStyleCSS\portefolio.css">
+    <title>laMeute</title>
+  </head>
+  <body>
+
+  <div class="header">
+        <nav class="navbar bg-light">
+          <div class="container-fluid">
+            <a class="navbar-title" href="#"> <span>Port</span>Folio</a>
+          </div>
+          <div class="contatainer-fluid2">
+              <a class="navbar-items" href="#">About</a>
+              <a class="navbar-items" href="#">Our Projects</a>
+              <a class="navbar-items" href="#">Contact Us</a>
+              <a class="navbar-items" href="#">Skills</a>
+          </div>
+        </nav>
+
+      <div class="body1">
+          <div class="body1-left">
+              <p> LaMeute </p>
+              <p> We are a group specialized in computer service such as software programming web programming , mobile app builds, graphic design, 3D modeling and counting... </p>
+              <div class="body-icon">  </div>
+          </div>
+          <div class="body1_right">
+            <img src="..\assets\images\blackblue.png" alt="" class="img-logo">
+          </div>
+      </div>
+
+  </div>
+
+  </body>
+</html>
